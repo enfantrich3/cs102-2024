b6f3daeef8a56d8461ebe385a2f4a24e2b1c6875
Daria <enfantriche@MacBook-Air-Daria-2.local>
2025-01-11 16:29:30 +0300

    fix mistakes

--- homework02/test_sudoku.py
+++ homework02/test_sudoku.py
@@ -240,5 +240,7 @@ class SudokuTestCase(unittest.TestCase):
         solution = sudoku.solve(grid)
         solved = sudoku.check_solution(solution)
         self.assertTrue(solved)
+
+
 if __name__ == "__main__":
     unittest.main()
