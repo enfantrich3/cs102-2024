5028e9574db6da7e5179807ca3f535307eff2865
Daria <enfantriche@MacBook-Air-Daria-2.local>
2025-01-11 16:11:01 +0300

    fix mistakes

--- homework01/tests/test_caesar.py
+++ homework01/tests/test_caesar.py
@@ -51,5 +51,7 @@ class CaesarTestCase(unittest.TestCase):
             caesar.decrypt_caesar(ciphertext, shift=shift),
             msg=f"shift={shift}, ciphertext={ciphertext}",
         )
+
+
 if __name__ == "__main__":
     unittest.main()
